c14d578a68e6d29f2515af028b7824a0a1a6189f
Morgan Sayers <morgansayers20298@gmail.com>
2023-03-30 14:56:26 -0500

    Add project files.

---
+++ Controllers/SlimeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Slimepen.Controllers
+{
+    public class SlimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
